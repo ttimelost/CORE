7b2a5d89a1b3a4bddf5d6df92e2eefa0ec1ca444
ttimelost <arthurbirencastro@gmail.com>
2026-03-26 22:37:51 -0300

    Mudei o texto

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>CORE - Investimento seguro</Text>
+      <Text>Mudei o nome de novo !!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
