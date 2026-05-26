1c06681bcd3889a2e908d1b780f2cad2d29c0c4d
Vitor Barsali <vitor.carvalho@ort.org.br>
2026-05-26 12:24:27 -0300

    Implementa campo de entrada para nome e botão de validação

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [nome, setNome] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Core</Text>
-      <StatusBar style="auto" />
+     <View style={styles.container}>
+      <TextInput 
+        placeholder='Digite seu nome'
+        onChangeText={setNome}
+        value={nome}
+        style={{borderWidth: 1, width: 200, marginVertical: 10, padding: 5}}/>
+
+      {/*Exibe o Text apenas se a variavel erro for diferente de undefined*/}
+      {{erro} && <Text style={{color: 'red'}}>{erro}</Text>}
+      
+      <Button title='Validar' onPress={validar}/>
     </View>
   );
 }
